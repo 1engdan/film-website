e2582c0ec69d060f91b40b8ef5564d6c8e1c61a2
1engdan <btw1engdan@gmail.com>
2025-10-05 18:31:28 +0900

    added ui loading

---
+++ src/components/ui/loading/Loading.tsx
@@ -0,0 +1,13 @@
+import './loading.css';
+
+const LoadingSpinner = () => {
+    return (
+        <div className="loader">
+            <div className="inner one"></div>
+            <div className="inner two"></div>
+            <div className="inner three"></div>
+        </div>
+    );
+};
+
+export default LoadingSpinner
